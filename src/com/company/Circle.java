bd611872f672e950695d3122d9ef4e8e63bfd83e
AndriyPlytka <andriyplutka@gmail.com>
2022-07-03 19:32:10 +0300

    PackegeScopes

---
+++ src/com/company/Circle.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Circle {
+    int radius = 7 ;
+    private static final float P = 3.14f;
+
+    public static float calcArea(int r){
+        return P*(r*r);
+
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Circle obj1 = new Circle();
+        //Circle.CalcArea();
+        //circle.CalcArea();
+//        obj1.radius=4;
+        System.out.println("Area4 = " + obj1.calcArea(7));
+
+//        obj1.radius=7;
+//        System.out.println("Area7 = " + obj1.calcArea());
+
+
+
+
+    }
+
+}
